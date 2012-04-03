4f5db71ab311f4dedae3fbd07abd9ff2ad8904c2
Mike Walsh <info@imaginalgo.com>
2012-04-03 10:32:46 -0400

    BlueBubble 3.5 release.
    Replaced Shortcodes with styles in the Wordpress Text Editor to prevent dirty little [shortcodes] from being left behind in case a theme is changed.

--- BlueBubble/single.php
+++
@@ -1,7 +0,0 @@
-<?php $bb_portfolio_cat = get_option('bb_portfolio_cat');
-if ( in_category($bb_portfolio_cat) ) {
-  include(TEMPLATEPATH . '/single_portfolio.php');
-} else {
-  include(TEMPLATEPATH . '/single_blog.php');
-}
-?>
